325845ad4b2052df7e1bfcd2224522fd29fcb62d
Arden Zhu <420395671@qq.com>
2016-12-28 00:08:31 +0800

    Frame

--- ColorsApp/ColorsApp/ReflectedColorsPage.cs
+++ ColorsApp/ColorsApp/ReflectedColorsPage.cs
@@ -15,6 +15,7 @@ namespace ColorsApp
             StackLayout stackLayout = new StackLayout
             {
                 BackgroundColor = Color.Blue,
+                //HorizontalOptions = LayoutOptions.Start,
             };
             // Loop through the Color structure fields.
             foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
@@ -27,7 +28,8 @@ namespace ColorsApp
                 info.FieldType == typeof(Color))
                 {
                     stackLayout.Children.Add(
-                    CreateColorLabel((Color)info.GetValue(null), info.Name));
+                    //CreateColorLabel((Color)info.GetValue(null), info.Name));
+                    CreateColorView((Color)info.GetValue(null), info.Name));
                 }
             }
             // Loop through the Color structure properties.
@@ -39,7 +41,8 @@ namespace ColorsApp
                 methodInfo.ReturnType == typeof(Color))
                 {
                     stackLayout.Children.Add(
-                    CreateColorLabel((Color)info.GetValue(null), info.Name));
+                    //CreateColorLabel((Color)info.GetValue(null), info.Name));
+                    CreateColorView((Color)info.GetValue(null), info.Name));
                 }
             }
             Padding = new Thickness(5, Device.OnPlatform(20, 5, 5), 5, 5);
@@ -48,6 +51,7 @@ namespace ColorsApp
             {
                 Content = stackLayout,
                 BackgroundColor = Color.Red,
+                //VerticalOptions = LayoutOptions.Start,
             };
         }
         Label CreateColorLabel(Color color, string name)
@@ -67,7 +71,62 @@ namespace ColorsApp
                 Text = name,
                 TextColor = color,
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                BackgroundColor = backgroundColor
+                BackgroundColor = backgroundColor,
+                //HorizontalOptions = LayoutOptions.Start,
+            };
+        }
+
+        View CreateColorView(Color color, string name)
+        {
+            return new Frame
+            {
+                OutlineColor = Color.Accent,
+                Padding = new Thickness(5),
+                Content = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    Spacing = 15,
+                    Children =
+                    {
+                    new BoxView
+                    {
+                    Color = color
+                    },
+                    new Label
+                    {
+                    Text = name,
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                    FontAttributes = FontAttributes.Bold,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.StartAndExpand
+                    },
+                    new StackLayout
+                    {
+                    Children =
+                    {
+                    new Label
+                    {
+                    Text = String.Format("{0:X2}-{1:X2}-{2:X2}",
+                    (int)(255 * color.R),
+                    (int)(255 * color.G),
+                    (int)(255 * color.B)),
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    IsVisible = color != Color.Default
+                    },
+                    new Label
+                    {
+                    Text = String.Format("{0:F2}, {1:F2}, {2:F2}",
+                    color.Hue,
+                    color.Saturation,
+                    color.Luminosity),
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    IsVisible = color != Color.Default
+                    }
+                    },
+                    HorizontalOptions = LayoutOptions.End
+                    }
+                    }
+                }
             };
         }
     }
